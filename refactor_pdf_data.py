e5f514392f57d5fbedc9dcd2d2798443cd21e1f4
Yogesh Sherawat <yogeshwar.sherawat@kukufm.com>
2023-09-18 12:22:17 +0530

    removed xtra file

--- refactor_pdf_data.py
+++
@@ -1,9 +0,0 @@
-def refactor_data(pages):
-    """
-    This refactoring function would be different for each pdf file, so change accordingly
-    """
-    page_texts = []
-    for page in pages:
-        page_text = page.replace("\t", " ").replace("\n", " ")
-        page_texts.append(page_text)
-    return page_texts
